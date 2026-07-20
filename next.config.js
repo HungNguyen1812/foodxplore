61bd238e54550d681ed7f8f438d8c67b8885777c
Hung Nguyen <kimkurosaki1999@gmail.com>
2026-07-20 14:50:25 +0700

    Allow RSS image domains

--- next.config.js
+++ next.config.js
@@ -2,19 +2,46 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'picsum.photos' },
-      { protocol: 'https', hostname: '**.vnexpress.net' },
-      { protocol: 'https', hostname: '**.cafef.vn' },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.vnexpress.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.vnecdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cafef.vn',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.vnncdn.net',
+      },
     ],
   },
+
   async headers() {
     return [
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, OPTIONS',
+          },
         ],
       },
     ];
